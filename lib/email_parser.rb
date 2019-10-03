10cdd0c683914cda2557a64c5ab97691dbf5bdd8
Shamel Lakin <christlife4u@gmail.com>
2019-10-03 17:28:24 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,21 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser 
+  attr_accessor  :csv_emails
+  
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end 
+	
+	def parse
+		@csv_emails = @csv_emails.gsub(/[\s,]/ ," ").split
+		# @csv_emails is a instance variable,The gsub string method is a substitution method,It finds all instances of the matched string and replaces it with the new argument. /[\s] removes white space. 
+		
+		@csv_emails.flatten.uniq
+		#.uniq Returns a new array by removing duplicate values in self. .flatten Returns a new array that is a one-dimensional flattening of self (recursively).That is, for every element that is an array, extract its elements into the new array.
+	end
+	
+end
+
